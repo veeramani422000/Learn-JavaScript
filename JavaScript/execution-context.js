1badb8f73998ccb8a6d114def098cc96d8018526
Veeramani Marimuthu <118247114+veeramanimarimuthu@users.noreply.github.com>
2024-01-21 13:27:39 +0530

    Add files via upload

---
+++ JavaScript/execution-context.js
@@ -0,0 +1,10 @@
+/* 
+    1. How JavaScript work?
+        "Everything in JavaScript happens inside an Execution Context."
+
+    2. Synchronous or Asynchronous? & Single Threaded or Multi-Threaded?
+        "JavaScript synchronous single-threaded (ss) language."
+
+*/
+
+
