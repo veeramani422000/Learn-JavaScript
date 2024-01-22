0002ba59d9854e23369c001a5bd374d30b514e0b
Veeramani Marimuthu <118247114+veeramanimarimuthu@users.noreply.github.com>
2024-01-22 20:10:59 +0530

    Add files via upload

---
+++ JavaScript/execution-context.js
@@ -0,0 +1,12 @@
+/* 
+    1. How JavaScript work?
+        Everything in JavaScript happens inside an Execution Context.
+        It is divided into two components. 
+        One is memory(Creation phase) and the other is code(Execution phase). 
+        code is executed one line at a time.
+
+    2. Synchronous or Asynchronous? & Single Threaded or Multi-Threaded?
+        "JavaScript synchronous single-threaded (ss) language."
+*/
+
+
